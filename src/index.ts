5cf105f65703d90a2359b32d2ade4588b2fac30e
__aqzhyi__ <pleasurazy@gmail.com>
2024-08-26 00:34:47 +0800

    build!: retire the old-fashioned build plan from 9 years ago and switch to TypeScript, Bun, and Vitest

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { tify } from '~/libs/tongwen-st'
+import { sify } from '~/libs/tongwen-ts'
+
+export { sify, tify }
+
+export default {
+  sify: sify,
+  tify: tify,
+}
---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '~': resolve(__dirname, 'src'),
+    },
+  },
+})
