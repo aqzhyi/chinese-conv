55dde23bf9744e28bbe37d5a2b90c2334a3a9536
__aqzhyi__ <pleasurazy@gmail.com>
2024-10-02 13:07:56 +0800

    test: adjust the scope of coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,16 @@
 /// <reference types="vitest" />
 import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   resolve: {
     alias: {
       '~': resolve(__dirname, 'src'),
+    },
+  },
+  test: {
+    coverage: {
+      exclude: ['*.mjs', '*.config.*', 'dist', '**/*.d.ts', '**/node_modules/**'],
     },
   },
 })
